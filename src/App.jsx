92dfd27e72f96e706a1e2ecd9ca9372bddfc2776
Tunzymono <tife2020@gmail.com>
2024-02-05 10:44:38 +0100

    login-Form mobile with components (Container, Input, LoginForm, NavBar)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
 
 const contDivStyle = {
   width: "940px",
   display: "flex",
   flexDirection: "column",
   gap: "32px",
-  backgroundColor: "blue",
+  
 };
 
 export default function App() {
@@ -16,7 +17,8 @@ export default function App() {
       <Container>
         <div style={contDivStyle}>
           <Navbar />
-          
+          <h1 className="logInH1">Log in</h1>
+          <LoginForm />
         </div>
       </Container>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
-import { Layout } from "antd";
 import todoistLogo from "../assets/todoist-logo.svg";
+import todoistIcon from "../assets/todoist-icon.svg";
+import { styled } from "styled-components";
 
-const { Header} = Layout;
 
-const headerStyle = {
-  backgroundColor: "white",
-  display:'flex',
-  alignItems:'center',
-  padding:'0'
-};
+const Header = styled.nav`
+  backgroundColor: white;
+  display: flex;
+  alignItems: center;
+  padding: 0;
+
+  & .deskLogo{
+      display:none;
+    }
+
+  @media (min-width:768px){
+    & .mobileLogo{
+      display:none;
+    }
+
+    & .deskLogo{
+      display:block;
+    }
+
+
+  }
+`;
+
 
 export default function Navbar() {
   return (
-        <Header style={headerStyle}>
-          <img src={todoistLogo}></img>
-        </Header>
+    <Header>
+      <img src={todoistIcon} className="mobileLogo"></img>
+      <img src={todoistLogo} className="deskLogo"></img>
+    </Header>
   );
 }
