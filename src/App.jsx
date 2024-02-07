de3dec7eeb7e9683f5ec506d54e54bd9c45cad91
Tunzymono <tife2020@gmail.com>
2024-02-08 00:51:12 +0100

    Moved log in page to a component to enable render via RouteProvider in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import "./App.css";
-import Container from "./components/Container";
-import Navbar from "./components/Navbar";
-import LoginForm from "./components/LoginForm";
+// import Container from "./components/Container";
+// import Navbar from "./components/Navbar";
+// import LoginForm from "./components/LoginForm";
 
-const contDivStyle = {
-  width: "940px",
-  display: "flex",
-  flexDirection: "column",
-  gap: "32px",
+// const contDivStyle = {
+//   width: "940px",
+//   display: "flex",
+//   flexDirection: "column",
+//   gap: "32px",
   
-  
+// };
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
 
-};
+const router = createBrowserRouter([
+  {path:'/', element: <Home /> }
+])
 
 export default function App() {
   return (
-    <div className = "container">
-      <Container>
-        <div style={contDivStyle}>
-          <Navbar />
-          <h1 className="logInH1">Log in</h1>
-          <LoginForm />
-        </div>
-      </Container>
-    </div>
+    // <div className = "container">
+    //   <Container>
+    //     <div style={contDivStyle}>
+    //       <Navbar />
+    //       <h1 className="logInH1">Log in</h1>
+    //       <LoginForm />
+    //     </div>
+    //   </Container>
+    // </div>
+    <>
+      <RouterProvider router = {router}/>
+    </>
   );
 }
